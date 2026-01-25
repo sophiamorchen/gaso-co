a4a527696a859265ee9cc32b74f072f3cb9c9925
Sophia M <sophia.moerchen@gmail.com>
2026-01-25 14:51:52 +0100

    ajout des _globals.scss (variables + fonts) ajout du json pour redirriger la compilation vers assets/css vs assets/scss modification du link assets/css/main.css

--- includes/header.php
+++ includes/header.php
@@ -10,7 +10,7 @@
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         
         <title>Gaso&co</title>
-        <link rel="stylesheet" href="<?=  BASE_URL ?>assets/scss/main.css">
+        <link rel="stylesheet" href="<?=  BASE_URL ?>assets/css/main.css">
         <script src="<?= BASE_URL ?>assets/js"></script>
     </head>
     
@@ -20,6 +20,7 @@
                 <ul>
                     <li><a href="">Accueil</a></li>
                     <li>Blog</li>
+                    <li>Contact</li>
                     <li>Contact</li>
                 </ul>
             </nav>
