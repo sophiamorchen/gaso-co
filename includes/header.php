81f6a1ea985f2948b87d806a9d586cee3e0e4e99
Sophia M <sophia.moerchen@gmail.com>
2026-01-25 19:46:25 +0100

    ajout style, bootstrap, carousel et cards et gitignore

--- includes/header.php
+++ includes/header.php
@@ -1,28 +1,47 @@
-<?php include_once 'config.php' 
+<?php include_once 'config.php'
 
 
 ?>
 <!DOCTYPE html>
 <html lang="en">
-    
-    <head>
-        <meta charset="UTF-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        
-        <title>Gaso&co</title>
-        <link rel="stylesheet" href="<?=  BASE_URL ?>assets/css/main.css">
-        <script src="<?= BASE_URL ?>assets/js"></script>
-    </head>
-    
-    <body>
-        <header>
-            <nav>
-                <ul>
-                    <li><a href="">Accueil</a></li>
-                    <li>Blog</li>
-                    <li>Contact</li>
-                    <li>Contact</li>
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <title>Gaso&co</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
+    <link rel="stylesheet" href="<?= BASE_URL ?>assets/css/main.css">
+    <script src="<?= BASE_URL ?>assets/js"></script>
+</head>
+
+<body>
+    <header>
+        <nav class="nav-top">
+            <div class="container">
+                <ul class="nav-list">
+                    <li><a href="">gaso.co64@gmail.com</a></li>
+                    <li><a href="">07.66.81.65.11</a></li>
+                    <li><a href="">2001 chemin de cassou, puyôo</a></li>
+                </ul>
+            </div>
+        </nav>
+
+        <div class="under-header container">
+            <div class="header-brand">
+                <h1 class="site-title"><a href="">GaSo&Co</a></h1>
+                <span class="site-tagline">ENTRETIEN, BRICOLAGE & LOCATION DE MATERIEL </span>
+            </div>
+
+            <nav class="nav-main">
+                <ul class="nav-list-main">
+                    <li><a href="<?= BASE_URL ?>">Accueil</a></li>
+                    <li><a href="">Prestation de services</a></li>
+                    <li><a href="">Location matériel</a></li>
+                    <li><a href="">Devis</a></li>
+                    <li><a href="">Contact</a></li>
                 </ul>
             </nav>
-        </header>
+        </div>
 
+    </header>
--- index.php
+++ index.php
@@ -4,6 +4,89 @@ include __DIR__ . '/includes/header.php';
 
 ?>
 <main>
-    Page
+    <section class="container">
+        <div id="carouselExampleCaptions" class="carousel slide">
+            <div class="carousel-indicators">
+                <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+                <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="1" aria-label="Slide 2"></button>
+                <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="2" aria-label="Slide 3"></button>
+            </div>
+            <div class="carousel-inner">
+                <div class="carousel-item active">
+                    <img src="<?= BASE_URL ?>assets/img/home-page-hero-bg.png" class="d-block w-100" alt="...">
+                    <div class="carousel-caption d-none d-md-block">
+                        <h5>Location de matériel professionnel </h5>
+                        <p>Du matériel fiable, performant et prêt à l’emploi pour vos chantiers,
+                            travaux ponctuels ou projets personnels. Louez simplement, au bon prix,
+                            et gagnez du temps.</p>
+                    </div>
+                </div>
+                <div class="carousel-item">
+                    <img src="<?= BASE_URL ?>assets/img/construction-hero-bg.png" class="d-block w-100" alt="...">
+                    <div class="carousel-caption d-none d-md-block">
+                        <h5>Travaux de bricolage et rénovation</h5>
+                        <p>Besoin d’un coup de main ou d’un savoir-faire professionnel ?
+                            Nous vous accompagnons dans vos travaux, du petit bricolage
+                            aux rénovations plus complètes.</p>
+                    </div>
+                </div>
+                <div class="carousel-item">
+                    <img src="<?= BASE_URL ?>assets/img/services-hero-bg.png" class="d-block w-100" alt="...">
+                    <div class="carousel-caption d-none d-md-block">
+                        <h5>Entretien espaces verts</h5>
+                        <p>Tonte, débroussaillage, entretien régulier ou remise en état :
+                            nous prenons soin de vos extérieurs pour un rendu propre,
+                            net et durable..</p>
+                    </div>
+                </div>
+            </div>
+            <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="prev">
+                <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                <span class="visually-hidden">Previous</span>
+            </button>
+            <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="next">
+                <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                <span class="visually-hidden">Next</span>
+            </button>
+        </div>
+    </section>
+
+    <section class="article-section">
+        <div class="container card-container">
+
+            <div class="card" style="width: 18rem;">
+                <img src="..." class="card-img-top" alt="...">
+                <div class="card-body">
+                    <h5 class="card-title">Card title</h5>
+                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card’s content.</p>
+                    <a href="#" class="btn btn-primary">Qui sommes nous ?</a>
+                </div>
+            </div>
+
+            <div class="card" style="width: 18rem;">
+                <img src="..." class="card-img-top" alt="...">
+                <div class="card-body">
+                    <h5 class="card-title">Card title</h5>
+                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card’s content.</p>
+                    <a href="#" class="btn btn-primary">Ce que nous faisons</a>
+                </div>
+            </div>
+
+            <div class="card" style="width: 18rem;">
+                <img src="..." class="card-img-top" alt="...">
+                <div class="card-body">
+                    <h5 class="card-title">Card title</h5>
+                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card’s content.</p>
+                    <a href="#" class="btn btn-primary">Pourquoi nous ?</a>
+                </div>
+            </div>
+
+        </div>
+    </section>
+
+
 </main>
-<?php include __DIR__ . '/includes/footer.php';
+<?php include __DIR__ . '/includes/footer.php'; ?>
+</body>
+
+</html>
