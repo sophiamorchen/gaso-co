c98db0b8204c5368551c6e80ec6ba6f3d7eaf4cd
Sophia M <sophia.moerchen@gmail.com>
2026-01-26 14:27:45 +0100

    aprsè erreur de commit depuis img , maintenant vrai commit de gasoetco

--- index.php
+++ index.php
@@ -1,92 +1,80 @@
 <?php
 
-include __DIR__ . '/includes/header.php';
+include_once __DIR__ . '/includes/header.php';
 
 ?>
 <main>
-    <section class="container">
-        <div id="carouselExampleCaptions" class="carousel slide">
-            <div class="carousel-indicators">
-                <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
-                <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="1" aria-label="Slide 2"></button>
-                <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="2" aria-label="Slide 3"></button>
-            </div>
-            <div class="carousel-inner">
-                <div class="carousel-item active">
-                    <img src="<?= BASE_URL ?>assets/img/home-page-hero-bg.png" class="d-block w-100" alt="...">
-                    <div class="carousel-caption d-none d-md-block">
-                        <h5>Location de matériel professionnel </h5>
-                        <p>Du matériel fiable, performant et prêt à l’emploi pour vos chantiers,
-                            travaux ponctuels ou projets personnels. Louez simplement, au bon prix,
-                            et gagnez du temps.</p>
-                    </div>
+    <div class="container">
+        <section class="caroussel-container">
+            <div id="carouselExampleCaptions" class="carousel slide">
+                <div class="carousel-indicators">
+                    <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+                    <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="1" aria-label="Slide 2"></button>
+                    <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="2" aria-label="Slide 3"></button>
                 </div>
-                <div class="carousel-item">
-                    <img src="<?= BASE_URL ?>assets/img/construction-hero-bg.png" class="d-block w-100" alt="...">
-                    <div class="carousel-caption d-none d-md-block">
-                        <h5>Travaux de bricolage et rénovation</h5>
-                        <p>Besoin d’un coup de main ou d’un savoir-faire professionnel ?
-                            Nous vous accompagnons dans vos travaux, du petit bricolage
-                            aux rénovations plus complètes.</p>
-                    </div>
-                </div>
-                <div class="carousel-item">
-                    <img src="<?= BASE_URL ?>assets/img/services-hero-bg.png" class="d-block w-100" alt="...">
-                    <div class="carousel-caption d-none d-md-block">
-                        <h5>Entretien espaces verts</h5>
-                        <p>Tonte, débroussaillage, entretien régulier ou remise en état :
-                            nous prenons soin de vos extérieurs pour un rendu propre,
-                            net et durable..</p>
-                    </div>
-                </div>
-            </div>
-            <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="prev">
-                <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-                <span class="visually-hidden">Previous</span>
-            </button>
-            <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="next">
-                <span class="carousel-control-next-icon" aria-hidden="true"></span>
-                <span class="visually-hidden">Next</span>
-            </button>
-        </div>
-    </section>
+                <div class="carousel-inner">
+                    <div class="carousel-item active">
+                        <img src="<?= BASE_URL ?>assets/img/home-page-hero-bg.png" class="d-block w-100" alt="...">
+                        <div class="carousel-caption d-none d-md-block">
+                            <h4>Location de matériel professionnel </h4>
 
-    <section class="article-section">
-        <div class="container card-container">
+                        </div>
+                    </div>
+                    <div class="carousel-item">
+                        <img src="<?= BASE_URL ?>assets/img/construction-hero-bg.png" class="d-block w-100" alt="...">
+                        <div class="carousel-caption d-none d-md-block">
+                            <h4>Travaux de bricolage et rénovation</h4>
+
+                        </div>
+                    </div>
+                    <div class="carousel-item">
+                        <img src="<?= BASE_URL ?>assets/img/services-hero-bg.png" class="d-block w-100" alt="...">
+                        <div class="carousel-caption d-none d-md-block">
+                            <h4>Entretien espaces verts</h4>
+                        </div>
+                    </div>
+                </div>
+                <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="prev">
+                    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                    <span class="visually-hidden">Previous</span>
+                </button>
+                <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="next">
+                    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                    <span class="visually-hidden">Next</span>
+                </button>
+            </div>
+        </section>
+
+        <section class="container card-container">
 
             <div class="card" style="width: 18rem;">
                 <img src="..." class="card-img-top" alt="...">
                 <div class="card-body">
-                    <h5 class="card-title">Card title</h5>
-                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card’s content.</p>
-                    <a href="#" class="btn btn-primary">Qui sommes nous ?</a>
+                    <h5 class="card-title">Disponible à la location</h5>
+                    <a href="#" class="btn btn-primary">Voir le matériel</a>
                 </div>
             </div>
-
+            <div class="card" style="width: 18rem;">
+                <img src="<?= BASE_URL ?>/assets/img/gants-tournevis.jpg" class="card-img-top" alt="photo gants tournevis">
+                <div class="card-body">
+                    <h5 class="card-title">Service de bricolage</h5>
+                    <a href="#" class="btn btn-primary">Voir les prestations</a>
+                </div>
+            </div>
             <div class="card" style="width: 18rem;">
                 <img src="..." class="card-img-top" alt="...">
                 <div class="card-body">
-                    <h5 class="card-title">Card title</h5>
-                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card’s content.</p>
-                    <a href="#" class="btn btn-primary">Ce que nous faisons</a>
+                    <h5 class="card-title">Espaces verts</h5>
+                    <a href="#" class="btn btn-primary">Voir les prestations</a>
                 </div>
             </div>
+        </section>
 
-            <div class="card" style="width: 18rem;">
-                <img src="..." class="card-img-top" alt="...">
-                <div class="card-body">
-                    <h5 class="card-title">Card title</h5>
-                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card’s content.</p>
-                    <a href="#" class="btn btn-primary">Pourquoi nous ?</a>
-                </div>
-            </div>
-
-        </div>
-    </section>
+    </div>
 
 
 </main>
-<?php include __DIR__ . '/includes/footer.php'; ?>
+<?php include_once __DIR__ . '/includes/footer.php'; ?>
 </body>
 
 </html>
